6f2659af9efa613c783aa4430853ac13f2a97386
fg776685-hue <fg776685@gmail.com>
2026-04-29 08:21:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+// Daftar produk dengan gambar
+const products = [
+    { id: 1, name: 'BENG-BENG', price: 2000, img: 'img/bb.jpg' },
+    { id: 2, name: 'BONCABE', price: 1000, img: 'img/bon.jpg' },
+    { id: 3, name: 'CHOCOPIE', price: 2000, img: 'img/cc.jpg' },
+    { id: 4, name: 'MAXICORN', price: 2000, img: 'img/max.jpg' },
+    { id: 5, name: 'QTELA', price: 2000, img: 'img/minuman.jpg' },
+    { id: 6, name: 'QTELA', price: 2000, img: 'img/qt.jpg' },
+
+
+];
+
+// keranjang belanja
+let cart = [];
+
+//Fungsi untuk menampilkan daftar produk
+funcetion displayproduct() {
+    const productContainer = document.getElementById('product');
+    productContainer.forEach(product = {
+        const productDiv = document.createElement('div')
+        productDiv.classList.add('product');
+        productDiv.innerHTML = `
+            <img src="#${product.img}" alt="${product.name}">
+            <h3>${product.name}</h3>
+            <p>${product.price}</p>
+            <button onclick="addToCart(${product.id})">Tambah ke karanjang</button>
+            `;
+            productsContainer.appendChild(productDiv);
+    });
+}
+
+// Fungsi untuk menambah product ke keranjang belanja
+function addToCart(productId) {
+    const product = products.find(p => p.id === productId);
+    const cartItem = cart.find(item => item.id === productId);
+
+    if (cartItem) {
+        cartItem.quantity += 1;
+    } else {
+        cart.push({ ...product, quantity: 1});
+    }
+
+    updatecart();
+}
+
+// Fungsi untuk menampilkan isi keranjang belanja
+function updatecart() {
+    const cartItemsContainer = document.getElementById('cart-itemd');
+    cartItemsContainer.innerHTML = '';
+
+    let totalprice = 0;
+    cart.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${item.name} x ${item.quantity} - Rp ${item.price * item.quantity}`;
+        cartItemsContainer.appendChiId(listItem);
+
+        totalPrice += item.price * item.quantity;
+    });
+
+    document.getElementById('total-price').textContent = totalprice;
+}
+
+//Fungsi untuk melakukan checkout
+function checkout() {
+    if (cart.length === 0) {
+        alert('Keranjang anda kosong.');
+        return;
+    }
+
+    const total = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+    const payment = prompt(`Total belanja Anda Rp ${total}. Masukkan jumlah pembayaran:`);
+
+    if (payment >= total) {
+        alert(`pembayaran berhasil! kembalian Anda: Rp ${payment - total}`);
+        cart = [];
+        updatecart();
+    } else {
+        alert('uang Anda tidak mencukupi.');
+    }
+}
+
+//Event listener untuk tombol checkout
+document.getElementById('checkout-btn').addEventListener('click', checkout);
+
+// Tampilkan produk saat halaman dimuat
+displayProducts();
